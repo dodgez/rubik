c21b1e3da88d0de49fc58b3212dd9c244e9bf0de
dodgez <zacharysdodge@gmail.com>
2019-09-27 09:11:53 -0700

    Add initial code to solve cube

--- index.ts
+++ index.ts
@@ -10,3 +10,36 @@ for (let i = 0; i < 5; ++i) {
 }
 
 console.log(`Solving ${moves}`);
+solve(cube);
+
+function solve(cube: Cube, moves: string[] = []) {
+  if (moves.length == 3) console.log(moves);
+  if (cube.isSolved()) {
+    console.log(moves);
+    return true;
+  }
+
+  if (moves.length <= 6) {
+    for (let side of 'fFbBrRlLuUdD') {
+      if (moves.length >= 1) {
+        if (side != moves[moves.length - 1] && side.toLowerCase() == moves[moves.length - 1].toLowerCase()) {
+          continue;
+        }
+
+        if (moves.length >= 2) {
+          if (side == moves[moves.length - 1] && side == moves[moves.length - 2]) {
+            continue;
+          }
+        }
+      }
+      moves.push(side);
+      cube.rotateSide(side);
+
+      if (solve(cube, moves)) return true;
+      
+      cube.rotateSide(side, false);
+      moves.pop();
+    }
+  }
+  return false;
+}
