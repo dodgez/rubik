ee36efa299feb669afb1472f8597cb7cff247fe0
dodgez <zacharysdodge@gmail.com>
2019-09-27 09:11:32 -0700

    Add initial code to scramble cube

--- index.ts
+++ index.ts
@@ -2,6 +2,11 @@ import Cube from './src/cube';
 
 let cube = new Cube();
 
-console.log(cube.cubes);
-cube.rotateSide('f');
-console.log(cube.cubes);
+let moves = [];
+for (let i = 0; i < 5; ++i) {
+  let side = Math.floor(Math.random() * 6);
+  cube.rotateSide('fbrlud'[side]);
+  moves.push('fbrlud'[side]);
+}
+
+console.log(`Solving ${moves}`);
